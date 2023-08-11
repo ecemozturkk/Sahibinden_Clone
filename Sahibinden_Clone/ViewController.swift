a07122979268bcfc6f512709c47e08c5149c89f9
Ecem Ozturk <ecemozturk704@gmail.com>
2023-08-11 20:14:59 +0300

    Initial Commit

---
+++ Sahibinden_Clone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Sahibinden_Clone
+//
+//  Created by Ecem Öztürk on 11.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
